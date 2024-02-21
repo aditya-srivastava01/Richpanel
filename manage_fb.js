d20899c96f2a3b766a0b3e70cf5c7e036d03b0c1
notsogoodaditya <79082526+notsogoodaditya@users.noreply.github.com>
2024-02-21 16:55:23 +0530

    check

--- manage_fb.js
+++ manage_fb.js
@@ -1,3 +1,39 @@
+function statusChangeCallback(response) {
+  console.log('statusChangeCallback');
+  console.log(response);
+  if (response.status === 'connected') {
+    testAPI();
+  } else {
+    document.getElementById('status').innerHTML = 'Please log into this webpage.';
+  }
+}
+
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
+
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '748002430612215', // Replace with your Facebook App ID
+    xfbml      : true,
+    version    : 'v19.0' // Update to the desired Graph API version
+  });
+
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+};
+
+function testAPI() {
+  console.log('Welcome! Fetching your information....');
+  FB.api('/me/accounts', function(response) { // 'accounts' endpoint fetches the list of pages
+    console.log('Successful login for: ' + response.name);
+    document.getElementById('status').innerHTML = 'Thanks for logging in, ' + response.name + '!';
+    console.log('Page list:', response.data); // Output the list of pages
+  });
+}
 window.fbAsyncInit = function() {
   FB.init({
     appId      : '748002430612215', // Replace with your Facebook App ID
@@ -52,22 +88,22 @@ function testAPI() {
   });
 }
 
-function sendTextMessage(pageId, accessToken, recipientId, messageText) {
-  FB.api(
-    `/${pageId}/messages`,
-    'POST',
-    {
-      recipient: {
-        id: recipientId
-      },
-      message: {
-        text: messageText
-      },
-      messaging_type: 'RESPONSE', // Use RESPONSE for messages in response to user actions
-      access_token: accessToken
-    },
-    function(response) {
-      console.log('Message sent:', response);
-    }
-  );
-}
+// function sendTextMessage(pageId, accessToken, recipientId, messageText) {
+//   FB.api(
+//     `/${pageId}/messages`,
+//     'POST',
+//     {
+//       recipient: {
+//         id: recipientId
+//       },
+//       message: {
+//         text: messageText
+//       },
+//       messaging_type: 'RESPONSE', // Use RESPONSE for messages in response to user actions
+//       access_token: accessToken
+//     },
+//     function(response) {
+//       console.log('Message sent:', response);
+//     }
+//   );
+// }
